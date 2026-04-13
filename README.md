25598fcfcc8fd3e20d1237edf732f44ad0213919
shiva <shivamwork60@gmail.com>
2026-04-14 01:03:12 +0530

    Fixed file issue

---
+++ README.md
@@ -0,0 +1,2 @@
+# BankManagementSystem
+The Bank Management System is a desktop-based application developed to simulate core banking operations such as account creation, transactions, and account management. The project focuses on implementing real-world banking functionalities using Java and database integration.
---
+++ src/bank/management/system/Con1.java
@@ -0,0 +1,18 @@
+package bank.management.system;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+import java.sql.*;
+public class Con {
+    Connection connection;
+    Statement statement;
+   public Con() {
+        try{
+             connection =DriverManager.getConnection("jdbc:mysql://localhost:3306/BankSystem","root","Shivam@2028");
+             statement =connection.createStatement();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
